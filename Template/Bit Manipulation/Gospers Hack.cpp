f77a89f15e03a50dc37500d19aa9c0c3f30ac457
izumijuro <aseuo0726@gmail.com>
2022-02-06 04:23:37 +0800

    update 1284

---
+++ Template/Bit Manipulation/Gospers Hack.cpp
@@ -0,0 +1,17 @@
+//
+// Created by Cauchy on 2022/2/6.
+//
+
+void GospersHack(int k, int n) {
+    int cur = (1 << k) - 1;
+    int limit = (1 << n);
+    while (cur < limit) {
+        /**
+         *  do something
+         */
+        int lb = cur & -cur;
+        int r = cur + lb;
+        cur = ((r ^ cur) >> __builtin_ctz(lb) + 2) | r;
+        // 或者 cur = ((r ^ cur) >> 2 / lb) | r;
+    }
+}
