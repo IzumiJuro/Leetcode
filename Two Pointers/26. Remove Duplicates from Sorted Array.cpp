1103e2e264d4e8e16bd1eb6b5d72039441ea8412
izumijuro <aseuo0726@gmail.com>
2022-02-24 09:42:12 +0800

    update 26

---
+++ Two Pointers/26. Remove Duplicates from Sorted Array.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        if (nums.size() == 0) return 0;
+        int slow = 0;
+        int fast = 1;
+        while (fast < nums.size()) {
+            if (nums[fast] != nums[slow]) {
+                slow++;
+                nums[slow] = nums[fast];
+            }
+            fast++;
+        }
+        return slow + 1;
+    }
+};
---
+++ Two Pointers/30. Substring with Concatenation of All Words.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<int> findSubstring(string s, vector<string>& words) {
+        // vector<int> res;
+        // int len = words[0].size();
+        // int count = words.size();
+        // unordered_map<string, int> search;
+
+        // for (auto& word: words) {
+        //     search[word]++;
+        // }
+
+        // for (int i = 0; i < len; i++) {
+        //     unordered_map<string, int> Map;
+        //     for (int j = 0; j < len;)
+        // }
+    }
+};
