41a73971042748c09932717e31054d282f52a94a
izumijuro <aseuo0726@gmail.com>
2022-03-03 22:54:03 +0800

    update 1802

--- Binary Search/1802. Maximum Value at a Given Index in a Bounded Array.cpp
+++ Binary Search/1802. Maximum Value at a Given Index in a Bounded Array.cpp
@@ -1,9 +1,7 @@
-//
-// Created by Cauchy on 2022/1/30.
-//
 #include <bits/stdc++.h>
 
 using namespace std;
+
 class Solution {
 public:
     int maxValue(int n, int index, int maxSum) {
@@ -11,7 +9,7 @@ public:
         int left = index;
         int right = index;
         int rest = maxSum - n;
-        while (left > 0 && right < n - 1) {
+        while (left > 0 || right < n - 1) {
             int len = right - left + 1;
 
             if (rest >= len) {
@@ -20,15 +18,11 @@ public:
                 left = max(0, left - 1);
                 right = min(n - 1, right + 1);
             }
-            else
+            else 
                 break;
         }
         ans += rest / n;
         return ans;
     }
+
 };
-int main() {
-    Solution solution;
-    vector<int> quantities{15,10,10};
-    solution.maxValue(4, 2, 6);
-}
